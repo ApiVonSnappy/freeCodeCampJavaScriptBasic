ec6c9396a661732b52f4789a66d58ac22bdd2a85
ApiVonSnappy <77690457+ApiVonSnappy@users.noreply.github.com>
2022-04-07 10:40:45 +0200

    Update cashRegister.js

--- cashRegister.js
+++ cashRegister.js
@@ -1,6 +1,27 @@
 function checkCashRegister(price, cash, cid) {
-  let change;
+  let change = cash - price;
+  const emptyChange = change => {
+  //while (change > 0) {
+    if (change => 100) {
+    
+    } else if (change => 20) {
+
+    } else if (change => 5) {
+
+    } else if (change => 1) {
+
+    } else if (change => 0.25) {
+
+    } else if (change => 0.1) {
+
+    } else if (change => 0.05) {
+
+    } else if (change => 0.01) {
+
+    }
+ // }
+  }
   return change;
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
+console.log(checkCashRegister(19.5, 119.5, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
